d45e92ce3e05dd968d72963f9c47b3e778a3d6eb
twinbird <ixa2063@gmail.com>
2021-11-12 15:05:10 +0900

    modify output format

--- main.go
+++ main.go
@@ -25,13 +25,13 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 func requestPrint(t time.Time, r *http.Request) string {
 	const log_time_format = "2006/01/02 15:04:05"
 
-	s := fmt.Sprintf("TIME: %s\n", t.Format(log_time_format))
-	s += fmt.Sprintf("URL: %s\n", r.URL)
-	s += fmt.Sprintf("Method: %s\n", r.Method)
-	s += fmt.Sprintf("Protocol: %s\n", r.Proto)
-	s += fmt.Sprintf("Header: %s\n", headerToString(r.Header, "\n\t"))
-	s += fmt.Sprintf("Body: %s\n", bodyToString(r, "\n\t"))
-	s += fmt.Sprintf("Parameters: %s\n", parsedParams(r, "\n\t"))
+	s := fmt.Sprintf("Time:\n\t%s\n", t.Format(log_time_format))
+	s += fmt.Sprintf("URL:\n\t%s\n", r.URL)
+	s += fmt.Sprintf("Method:\n\t%s\n", r.Method)
+	s += fmt.Sprintf("Protocol:\n\t%s\n", r.Proto)
+	s += fmt.Sprintf("Header:\n\t%s\n", headerToString(r.Header, "\n\t"))
+	s += fmt.Sprintf("Body:\n\t%s\n", bodyToString(r, "\n\t"))
+	s += fmt.Sprintf("Parameters:\n\t%s\n", parsedParams(r, "\n\t"))
 
 	return s
 }
@@ -48,7 +48,7 @@ func headerToString(r http.Header, sep string) string {
 
 func parsedParams(r *http.Request, sep string) string {
 	if err := r.ParseForm(); err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
 	s := ""
